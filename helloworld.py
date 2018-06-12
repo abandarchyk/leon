f553d1b75ca51dbf7c4a919707fedb93cf36ddf5
Aliaksandr B <aliaksandr.bondarchyk@gmail.com>
2018-06-12 19:55:47 +0300

    Wrap user input in pattern

--- helloworld.py
+++ helloworld.py
@@ -1,49 +1,77 @@
-import random
-import json
-import os
+import re
+
+
+lines = []
 
 
 def load_data():
-    songs_names = os.listdir("songs")
-    for current_song in songs_names:
-        file = open('songs/' + current_song, mode='r', encoding='utf-8')
-        song_in_lines = file.readlines()
-        return song_in_lines
-
-with open('song', mode='r', encoding='utf-8') as source:
-    dataList = json.load(source)
-    print(dataList)
+    file = open('data/songs/debug', mode='r', encoding='utf-8')
+    song_in_lines = file.readlines()
+    lines.extend(song_in_lines)
 
 
-while True:
-    resultList = []
-    print("you: ")
-    inputLine = input()
+def check_line(pattern, test_line: str):
+    print('Checking pattern: ' + pattern + ' in line: ' + test_line)
+    match = re.search(pattern, test_line)
+    return match
 
-    if inputLine == 'stop':
-        break
-    for currentElement in dataList:
-        topic = currentElement['topic']
-        patterns = currentElement['patterns']
-        for currentPattern in patterns:
-            result = inputLine.find(currentPattern)
-            if result != -1:
-                print("Found pattern: " + currentPattern)
-                topicPhrases = currentElement['phrases']
-                print("Adding topic to result list: " + topic)
-                resultList.extend(topicPhrases)
-                print('Result list is:')
-                print(resultList)
-                break
-    print('bot:')
-    if len(resultList) != 0:
-        index = random.randint(0, len(resultList)-1)
-        print(resultList[index])
-    else:
-        notheme = ['Разноцветные пташки за окном чирикали', 'Отразилась в огне, засверкала бликами',
-                   'Там, где пальмы растут и бананы, и агат']
-        index = random.randint(0, len(notheme) - 1)
-        print(notheme[index])
+
+def complete_line(match, test_line):
+    if match is not None:
+        start_index = match.start()
+        if start_index is 0:
+            end_index = match.end()
+            new_line = test_line[end_index: len(test_line)]
+            print(new_line)
+        else:
+            print(test_line)
+
+
+load_data()
+for current_line in lines:
+    user_input = 'она любила'
+    pat = r'\b' + user_input + r'\b'
+    match_result = check_line(pat, current_line)
+    print(match_result)
+    complete_line(match_result, current_line)
+
+
+
+# with open('song', mode='r', encoding='utf-8') as source:
+#     dataList = json.load(source)
+#     print(dataList)
+#
+#
+# def parse_line():
+#     resultList = []
+#     for words in resultList:
+#         print("you: ")
+#         inputLine = input()
+#
+#         if inputLine == 'stop':
+#             break
+#     for currentElement in dataList:
+#         topic = currentElement['topic']
+#         patterns = currentElement['patterns']
+#         for currentPattern in patterns:
+#             result = inputLine.find(currentPattern)
+#             if result != -1:
+#                 print("Found pattern: " + currentPattern)
+#                 topicPhrases = currentElement['phrases']
+#                 print("Adding topic to result list: " + topic)
+#                 resultList.extend(topicPhrases)
+#                 print('Result list is:')
+#                 print(resultList)
+#                 break
+#     print('bot:')
+#     if len(resultList) != 0:
+#         index = random.randint(0, len(resultList)-1)
+#         print(resultList[index])
+#     else:
+#         notheme = ['Разноцветные пташки за окном чирикали', 'Отразилась в огне, засверкала бликами',
+#                    'Там, где пальмы растут и бананы, и агат']
+#         index = random.randint(0, len(notheme) - 1)
+#         print(notheme[index])
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 
 all_lines = []
@@ -6,33 +7,54 @@ all_lines = []
 
 def load_data():
     songs_names = os.listdir("data/songs")
+    print('...Loading songs')
     for current_song in songs_names:
-        print('...Loading songs')
         print(current_song)
         file = open('data/songs/' + current_song, mode='r', encoding='utf-8')
         song_in_lines = file.readlines()
         all_lines.extend(song_in_lines)
 
 
-def complete_line(input_line: str):
-    for current_line in all_lines:
-        ind = current_line.find(input_line)
-        if ind != -1:
-            return current_line
-    return 'Not found'
+def check_line(pattern, test_line: str):
+    print('...Checking pattern: ' + pattern + ' in line: ' + test_line)
+    match = re.search(pattern, test_line, re.IGNORECASE)
+    if match is not None:
+        return True
+    return False
+
+
+def complete_line(match, test_line):
+    res = test_line
+    start_index = match.start()
+    if start_index is 0:
+        end_index = match.end()
+        res = test_line[end_index: len(test_line)]
+    return res
+
+
+def make_reply(input: str):
+#get match
+#if 0 - complete
+#if not 0 - whole string
+    # if match is None
+    # determine topic
 
 
 load_data()
+
+
 while True:
     print("you: ")
     user_input = input()
     if user_input == 'stop':
         break
-    result = complete_line(user_input)
-
-    print('bot:')
-    print(result)
-
+    pat = r'\b' + user_input + r'\b'
+    isFound = False;
+    for current_line in all_lines:
+        isFound = check_line(pat, current_line)
+        if isFound:
+            bot_reply = complete_line(matcher, current_line)
+            break
 
 
 
